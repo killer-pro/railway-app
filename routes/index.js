d710c1dbc3849f47c1e8fd53843d53586a868c87
Mouhamadou Diouf Cisse <cissemouhamadoudiouf@gmail.com>
2025-04-28 18:40:43 +0000

    pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const taskModel = require('../src/taskModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const tasks = await taskModel.getTasks();
+  res.render('index', { title: 'Gestionnaire de tâches', tasks });
 });
 
 module.exports = router;
